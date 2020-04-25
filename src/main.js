f60b48b2c22a0717ca555db18b762aed33aa59c4
halil burak cetinkaya <hburak3534@gmail.com>
2020-04-26 00:47:33 +0300

    automatic login and logging out fake key attempts.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,31 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
-import router from './router'
+import router from "./router";
+import Axios from "axios";
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
   router,
-  render: (h) => h(App)
+  created() {
+    // automatic login with local storage
+    const userString = localStorage.getItem("user");
+    if (userString) {
+      const userData = JSON.parse(userString);
+      this.$store.commit("SET_USER_DATA", userData);
+    }
+    // log out fake key attempts
+    Axios.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error.response.status === 401) {
+          this.$store.dispatch("logout");
+        }
+        return Promise.reject(error);
+      }
+    );
+  },
+  render: (h) => h(App),
 }).$mount("#app");
